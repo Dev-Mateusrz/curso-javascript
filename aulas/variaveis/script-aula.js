8ae8159606b71c9c7ed6fb2cba98c2ca2168f2a0
Mateus Rodrigues <devmateusrz@gmail.com>
2026-05-21 23:03:44 -0300

    Exercicio sobre Variáveis

---
+++ aulas/variaveis/script-aula.js
@@ -0,0 +1,37 @@
+var nome = "Mateus";
+var idade = 23;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprado = 5;
+var totalPreco = totalComprado * preco;
+
+console.log(totalPreco);
+
+var sobrenome = "Silva",
+  cidade = "São Paulo";
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+console.log(semDefinir);
+
+// console.log(aindaNaoDefinido);
+
+console.log(comida);
+var comida = "Pizza";
+
+/* Nomes podem iniciar com $, _, ou letras.
+Case Sensitive
+Não pode usar palavras Reservadas
+Camel case é usado para definir variáveis assim: abrirModal, fecharModal */
+
+
+/*
+const time = 'Vasco';
+time = 'Flamengo';
+
+console.log(time);
+*/
---
+++ aulas/variaveis/script-ex001.js
@@ -0,0 +1,17 @@
+// Declarar uma variável com o seu nome
+let nome = "Mateus";
+// Declarar uma variável com a sua idade
+let idade = 19;
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+let comidaFavorita;
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Pizza";
+// Declarar 5 variáveis diferentes sem valores
+let preco = 200,
+  dia,
+  possuiFaculdade,
+  time,
+  pais;
+
+console.log(nome, idade, comidaFavorita);
